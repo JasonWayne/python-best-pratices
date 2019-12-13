b5caa56d8109972d9d39dc8623f811b34f22f284
Wenjie Wu <wuwenjie0102@gmail.com>
2019-12-13 15:33:42 +0800

    Update builtin_vars.py

--- builtin_funcs/builtin_vars.py
+++ builtin_funcs/builtin_vars.py
@@ -1,10 +1,9 @@
 import argparse
- 
+
 FLAGS_BATCH_SIZE = 'batch_size'
 
 ap = argparse.ArgumentParser()
-ap.add_argument(FLAGS_BATCH_SIZE, required=True,
-	help="batch size")
+ap.add_argument(FLAGS_BATCH_SIZE, help="batch size")
 args = vars(ap.parse_args())
- 
+
 print('batch size is {}'.format(args[FLAGS_BATCH_SIZE]))
